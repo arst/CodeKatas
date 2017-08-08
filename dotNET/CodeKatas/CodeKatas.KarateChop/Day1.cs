b6f482a2d989bb0c777f988dbdfaf3df0b89d5c0
arst <artem.startsev@gmail.com>
2017-08-08 19:11:16 +0300

    KarateChop

---
+++ dotNET/CodeKatas/CodeKatas.KarateChop/Day1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.KarateChop
+{
+    public class SimpleBinarySearch
+    {
+        public int Search(int value, int[] input)
+        {
+            int start = 0;
+            int end = input.Length - 1;
+            int result = -1;
+
+            while (start <= end) {
+                var middle = (start + end) / 2;
+                if (value > input[middle])
+                {
+                    start = middle + 1;
+                }
+                else if (value < input[middle])
+                {
+                    end = middle - 1;
+                }
+                else {
+                    return middle;
+                }
+            }
+
+            return result;
+        }
+    }
+}
