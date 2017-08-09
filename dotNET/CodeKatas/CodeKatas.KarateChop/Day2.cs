f488dd6a4c2559c1c2a103bd970d178f367c36ec
arst <artem.startsev@gmail.com>
2017-08-09 21:02:21 +0300

    KarateChop CodeKata: BinarySearch.Day2: Recursive search

---
+++ dotNET/CodeKatas/CodeKatas.KarateChop/Day2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.KarateChop
+{
+    public class RecursiveBinarySearch
+    {
+        public int Search(int value, int[] input)
+        {
+            return SearchRecursive(value, input, 0, input.Length - 1);
+        }
+
+        private int SearchRecursive(int value, int[] input, int start, int end)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+            if (start == end)
+            {
+                return input[start] == value ? start : -1;
+            }
+        
+            var mid = (start + start + end) / 2;
+            if (value == input[mid])
+            {
+                return mid;
+            }
+            else if (value < input[mid])
+            {
+                return SearchRecursive(value, input, start, mid - 1);
+            }
+            else {
+                return SearchRecursive(value, input, mid + 1, end);
+            }
+        }
+    }
+}
---
+++ dotNET/CodeKatas/CodeKatas.KarateChopTests/RecursiveBinarySearchTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKatas.KarateChop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.KarateChop.Tests
+{
+    [TestClass()]
+    public class RecursiveBinarySearchTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            RecursiveBinarySearch day2Search = new RecursiveBinarySearch();
+            Assert.AreEqual(-1, day2Search.Search(3, new int[0]));
+            Assert.AreEqual(-1, day2Search.Search(3, new int[1] { 1 }));
+            Assert.AreEqual(0, day2Search.Search(1, new int[1] { 1 }));
+            Assert.AreEqual(0, day2Search.Search(1, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(1, day2Search.Search(3, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(2, day2Search.Search(5, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day2Search.Search(0, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day2Search.Search(2, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day2Search.Search(4, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day2Search.Search(6, new int[3] { 1, 3, 5 }));
+
+            Assert.AreEqual(0, day2Search.Search(1, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(1, day2Search.Search(3, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(2, day2Search.Search(5, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(3, day2Search.Search(7, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day2Search.Search(0, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day2Search.Search(2, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day2Search.Search(4, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day2Search.Search(6, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day2Search.Search(8, new int[4] { 1, 3, 5, 7 }));
+        }
+    }
+}
