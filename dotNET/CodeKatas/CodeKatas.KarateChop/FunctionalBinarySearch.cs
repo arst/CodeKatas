ef44ccbecc68a8ba59b2ae996e976c11fc0d5780
arst <artem.startsev@gmail.com>
2017-08-10 23:31:31 +0300

    KarateChop CodeKata: BinarySearch.Day3: Functional style with pattern matching

---
+++ dotNET/CodeKatas/CodeKatas.KarateChop/FunctionalBinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.KarateChop
+{
+    public class FunctionalBinarySearch
+    {
+        public int Search(int value, int[] input)
+        {
+            return SearchInternal(value, input, 0, input.Length - 1);
+        }
+
+        private int SearchInternal(int value, int[] input, int start, int end)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+            if(start == end)
+            {
+                return input[start] == value ? start : -1;
+            }
+            var mid = (start + start + end) / 2;
+            switch (mid)
+            {
+                case var i when input[i] == value:
+                    return mid;
+                case var i when input[i] > value:
+                    return SearchInternal(value, input, start, mid - 1);
+                case var i when input[i] < value:
+                    return SearchInternal(value, input, mid + 1, end);
+                default:
+                    return -1;
+            }
+        }
+    }
+}
---
+++ dotNET/CodeKatas/CodeKatas.KarateChopTests/FunctionalBinarySearchTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKatas.KarateChop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.KarateChop.Tests
+{
+    [TestClass()]
+    public class FunctionalBinarySearchTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            FunctionalBinarySearch day3Search = new FunctionalBinarySearch();
+            Assert.AreEqual(-1, day3Search.Search(3, new int[0]));
+            Assert.AreEqual(-1, day3Search.Search(3, new int[1] { 1 }));
+            Assert.AreEqual(0, day3Search.Search(1, new int[1] { 1 }));
+            Assert.AreEqual(0, day3Search.Search(1, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(1, day3Search.Search(3, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(2, day3Search.Search(5, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day3Search.Search(0, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day3Search.Search(2, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day3Search.Search(4, new int[3] { 1, 3, 5 }));
+            Assert.AreEqual(-1, day3Search.Search(6, new int[3] { 1, 3, 5 }));
+
+            Assert.AreEqual(0, day3Search.Search(1, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(1, day3Search.Search(3, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(2, day3Search.Search(5, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(3, day3Search.Search(7, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day3Search.Search(0, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day3Search.Search(2, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day3Search.Search(4, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day3Search.Search(6, new int[4] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, day3Search.Search(8, new int[4] { 1, 3, 5, 7 }));
+        }
+    }
+}
